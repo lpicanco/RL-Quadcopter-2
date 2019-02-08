e08d246e25856397e6e3babeea5b8e5110125166
Luiz Picanço <lpicanco@gmail.com>
2019-02-07 23:11:43 -0200

    replay_buffer

---
+++ agents/replay_buffer.py
@@ -0,0 +1,17 @@
+from collections import deque, namedtuple
+import random
+
+class ReplayBuffer:
+    def __init__(self, buffer_size):
+        self.buffer = deque(maxlen=buffer_size)
+        self.Experience = namedtuple("Experience", ["state", "action", "reward", "next_state", "done"])
+    
+    def add(self, state, action, reward, next_state, done):
+        xp = self.Experience(state, action, reward, next_state, done)
+        self.buffer.append(xp)
+
+    def size(self):
+        return len(self.buffer)
+
+    def sample(self, count):
+        return random.sample(self.buffer, count)
